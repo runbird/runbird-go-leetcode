ae6f340ae62fba3f3331237fcea7206ad0a0bb38
suocaiyuan <suocaiyuan@163.com>
2020-08-14 12:55:24 +0800

    leetcode-20

---
+++ stack/validParentheses.go
@@ -0,0 +1,58 @@
+package main
+
+//给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+//
+//有效字符串需满足：
+//
+//左括号必须用相同类型的右括号闭合。
+//左括号必须以正确的顺序闭合。
+//注意空字符串可被认为是有效字符串。
+//
+//示例 1:
+//
+//输入: "()"
+//输出: true
+//示例 2:
+//
+//输入: "()[]{}"
+//输出: true
+//示例 3:
+//
+//输入: "(]"
+//输出: false
+//示例 4:
+//
+//输入: "([)]"
+//输出: false
+//示例 5:
+//
+//输入: "{[]}"
+//输出: true
+//
+//来源：力扣（LeetCode）20
+//链接：https://leetcode-cn.com/problems/valid-parentheses
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+func isValid(s string) bool {
+	n := len(s)
+	if n%2 == 1 {
+		return false
+	}
+	dict := map[byte]byte{
+		')': '(',
+		']': '[',
+		'}': '{',
+	}
+	stack := []byte{}
+	for i := 0; i < n; i++ {
+		if dict[s[i]] > 0 {
+			if len(stack) == 0 || stack[len(stack)-1] != dict[s[i]] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, s[i])
+		}
+	}
+	return len(stack) == 0
+}
