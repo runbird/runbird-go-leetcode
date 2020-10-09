0b817ef5bd8e9130f15ba65c2cac50031906b115
suocaiyuan <suocaiyuan@163.com>
2020-10-09 14:35:47 +0800

    leetcode - twoSum

---
+++ array/twoSum.go
@@ -0,0 +1,58 @@
+package main
+
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+//
+//你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+//
+//
+//
+//示例:
+//
+//给定 nums = [2, 7, 11, 15], target = 9
+//
+//因为 nums[0] + nums[1] = 2 + 7 = 9
+//所以返回 [0, 1]
+//
+//来源：力扣（LeetCode）1
+//链接：https://leetcode-cn.com/problems/two-sum
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+//暴力法 1
+func twoSum(nums []int, target int) []int {
+	if nums == nil {
+		return []int{}
+	}
+
+	for i := 0; i < len(nums)-1; i++ {
+		for j := 1; j < len(nums); j++ {
+			if target-nums[i] == nums[j] {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+//暴力法 2
+func twoSum2(nums []int, target int) []int {
+	for i, x := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			if x+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+//字典法
+func twoSum3(nums []int, target int) []int {
+	hashTable := map[int]int{}
+	for i, x := range nums {
+		if p, ok := hashTable[target-x]; ok {
+			return []int{p, i}
+		}
+		hashTable[x] = i
+	}
+	return nil
+}
