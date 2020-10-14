e21a70d5e14b4c209812963c6a3e6cb19cce1644
suocaiyuan <suocaiyuan@163.com>
2020-10-14 16:53:36 +0800

    leetcode - array & listnode

---
+++ linkedList/swapNodesInPairs.go
@@ -0,0 +1,50 @@
+package main
+
+//给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+//你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+//
+//示例:
+//给定 1->2->3->4, 你应该返回 2->1->4->3.
+//
+//来源：力扣（LeetCode）24
+//链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+//递归写法
+//如果链表中至少有两个节点，则在两两交换链表中的节点之后，原始链表的头节点变成新的链表的第二个节点，原始链表的第二个节点变成新的链表的头节点。
+//链表中的其余节点的两两交换可以递归地实现。在对链表中的其余节点递归地两两交换之后，更新节点之间的指针关系，即可完成整个链表的两两交换。
+//head 表示原始链表的头节点，新的链表的第二个节点
+//newHead 表示新的链表的头节点，原始链表的第二个节点
+//原始链表中的其余节点的头节点是 newHead.next
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	newHead := head.Next
+	head.Next = swapPairs(newHead.Next)
+	newHead.Next = head
+	return newHead
+}
+
+//非递归
+//    /**
+//     *  核心代码
+//     *   temp.next = node2
+//     *   node1.next = node2.next
+//     *   node2.next = node1
+//     * */
+func swapPairs2(head *ListNode) *ListNode {
+	dummyHead := &ListNode{0, head}
+	temp := dummyHead
+	for temp.Next != nil && temp.Next.Next != nil {
+		node1 := temp.Next
+		node2 := temp.Next.Next
+
+		temp.Next = node2
+		node1.Next = node2.Next
+		node2.Next = node1
+
+		temp = node1
+	}
+	return dummyHead.Next
+}
